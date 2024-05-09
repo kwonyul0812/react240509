7930c01f33419a87a00597b4d285e1defe414201
kwonyul <kw990812@gmail.com>
2024-05-09 14:43:47 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,4 @@
 function App() {
-  // javascript review
-
-  // javascript data type
-  let a; // 값이 할당되지 않으면 undefined
-  console.log("a", typeof a);
-  let b = "문자열 타입"; // string
-  console.log("b", typeof b);
-  let c = 3; // number
-  console.log("c", typeof c);
-  let d = 3.14; // number
-  console.log("d", typeof d);
-  let e = true; // boolean
-  console.log("e", typeof e);
-
-  let f = {}; // object
-  console.log("f", typeof f);
-
-  let g = function () {}; // function
-  console.log("g", typeof g);
-  // 스크립트는 값의 타입을 미리 지정하는게 아닌
-  // 들어가는 값에 따라 변수의 타입이 바뀐다.
-
   return <div></div>;
 }
 
