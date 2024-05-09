465a57ee9ab4a872eed1ffd19e0c6df629a3b01d
kwonyul <kw990812@gmail.com>
2024-05-09 11:10:26 +0900

    컴포넌트 만들기 연습 끝

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+function MyTag() {
+  const a = <h3> hello component </h3>;
+  return a;
+}
+
+function MyContent() {
+  return <p> my content </p>;
+}
+
 function App() {
   return (
     <>
       <div>
         <h1>hello react</h1>
+        <MyTag />
+        <MyTag />
+        {/* <h3> hello component </h3> */}
+        <MyContent />
+        <MyContent />
+        {/* <p> my content </p> */}
       </div>
     </>
   );
