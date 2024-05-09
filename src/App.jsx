acf1874b387e7e510d505dbb9b4079339b624d0f
kwonyul <kw990812@gmail.com>
2024-05-09 11:37:41 +0900

    컴포넌트 만들기 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-function MyTag() {
-  return;
+function MyHeader() {
+  return (
+    <div>
+      <h1> hello header </h1>
+      <h3> hello sub header</h3>
+    </div>
+  );
+}
+
+function MyFooter() {
+  return (
+    <ul>
+      <li>link1</li>
+      <li>link2</li>
+    </ul>
+  );
+}
+
+// 다른  컴포넌트 안에 컴포넌트를 작성하면 안됨
+function MyNav() {
+  return (
+    <>
+      <a href="#">link1</a>
+      <a href="#">link2</a>
+    </>
+  );
 }
 
 function App() {
-  return;
+  return (
+    <div>
+      <MyHeader />
+      <MyFooter />
+      <MyNav />
+    </div>
+  );
 }
 
 export default App;
