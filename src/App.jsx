191eeb53229b52d712f7bc8da6ce2e392e7e13aa
kwonyul <kw990812@gmail.com>
2024-05-10 16:32:01 +0900

    style props 응용 연습완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+function MyButton({ name, sx }) {
+  return <button style={sx}>{name}</button>;
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      {/*  두 개의 MyButton 컴포넌트를 사용 sx, name */}
+      <MyButton
+        name="버튼1"
+        sx={{
+          backgroundColor: "lightblue",
+        }}
+      />
+      <MyButton
+        name="버튼2"
+        sx={{
+          backgroundColor: "coral",
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
