3d1e5059863a2b92fbef2e75ec0e630fb3829347
kwonyul <kw990812@gmail.com>
2024-05-10 10:17:48 +0900

    onClick property와 함수 사용 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  function action1() {
+    console.log("action1");
+  }
+
+  const action2 = function () {
+    console.log("action2");
+  };
+
+  const action4 = () => console.log("aciton4");
+
+  return (
+    <div>
+      <button onClick={action1}>click1</button>
+      <button onClick={action2}>click2</button>
+      <button
+        onClick={function () {
+          console.log("action3");
+        }}
+      >
+        click3
+      </button>
+      <button onClick={action4}>click4</button>
+      <button onClick={() => console.log("action5")}>click5</button>
+    </div>
+  );
 }
 
 export default App;
