8f9f1e2a1ca0e4290ad75a5e069dd867ef06b657
kwonyul <kw990812@gmail.com>
2024-05-10 16:09:27 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <div
-        style={{
-          color: "blue",
-          padding: "10px",
-          border: "2px dotted green",
-          textAlign: "center", // lowerCamelCase로 값 줄것
-          backgroundColor: "aqua",
-        }}
-      >
-        Lorem ipsum
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
