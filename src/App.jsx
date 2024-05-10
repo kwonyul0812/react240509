7116a19c7389003a715021211fdcf4d72ea0f5fc
kwonyul <kw990812@gmail.com>
2024-05-10 14:23:16 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import React from "react";
 
-function MyComp(props) {
-  console.log(props);
-  return (
-    <div>
-      <p>{props.name}</p>
-      <p>{props.age}</p>
-      <p>{props.address}</p>
-      <p>{props.city}</p>
-      <p>{props.country}</p>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name="son" age={33} address="seoul" />
-      <MyComp city="city" country="USA" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
