868f973e54f8acaa669328417aac2bb913da9dfd
kwonyul <kw990812@gmail.com>
2024-05-10 16:08:50 +0900

    style props은 객체로
    css property 명은
    lowerCamelCase 로

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div
+        style={{
+          color: "blue",
+          padding: "10px",
+          border: "2px dotted green",
+          textAlign: "center", // lowerCamelCase로 값 줄것
+          backgroundColor: "aqua",
+        }}
+      >
+        Lorem ipsum
+      </div>
+    </div>
+  );
 }
 
 export default App;
