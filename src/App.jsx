48382eca1b11f0998a462c8f612b9a13fc18ff53
kwonyul <kw990812@gmail.com>
2024-05-10 14:42:58 +0900

    props 의 값이 없을 때 기본값 할당하는 방법

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+function MyComp({ name, age, team = "무소속" }) {
+  return (
+    <div>
+      <ul>
+        <li>이름 : {name}</li>
+        <li>나이 : {age}</li>
+        <li>팀 : {team}</li>
+      </ul>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp name="흥민" age={33} team="토트넘" />
+      <MyComp name="강인" age={22} team="파리" />
+      <MyComp name="길동" age={44} />{" "}
+      {/* team 값을 안넘겨 주었기때문에 undefined */}
+    </div>
+  );
 }
 
 export default App;
