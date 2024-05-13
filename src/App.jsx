77b3fb621e9e420716ab8e18ecdd3d15a10c94db
kwonyul <kw990812@gmail.com>
2024-05-13 15:03:00 +0900

    state 연습 끝

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  // input에 입력된 값이 p에 출력되도록 코드 작성
+  const [text, setText] = useState("");
+
+  function updateText(e) {
+    setText(e.target.value);
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={updateText} />
+      <p>{text}</p>
+    </div>
+  );
 }
 
 export default App;
