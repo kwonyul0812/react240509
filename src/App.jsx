6dd326d9954a37ea27407167f7839f9309c5b4ae
kwonyul <kw990812@gmail.com>
2024-05-16 16:28:14 +0900

    응답 코드에 따라 다른 일을 하도록 함

--- src/App.jsx
+++ src/App.jsx
@@ -38,11 +38,27 @@ function App(props) {
       });
   }
 
+  function handleResponse() {
+    axios
+      .get("/api/main43/sub1")
+      .then((res) => console.log("정상적으로 응답 했습니다."))
+      .catch((err) => {
+        if (err.response.status === 401) {
+          console.log("401에러 발생");
+        } else if (err.response.status === 403) {
+          console.log("403에러 발생");
+        }
+      });
+  }
+
   return (
     <div>
       <button onClick={handle200}>200응답</button>
       <button onClick={handle400}>400응답</button>
       <button onClick={handle500}>500응답</button>
+
+      {/* 200, 401, 403 응답일 때 각각 다른 일하는 코드 작성*/}
+      <button onClick={handleResponse}>응답 코드에 따라 다른 일하기</button>
     </div>
   );
 }
