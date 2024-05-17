4cb9fc6fbba5cb061c1dcb617b418675d34f473e
kwonyul <kw990812@gmail.com>
2024-05-17 15:01:54 +0900

    공통된 상위로 묶기 연습완료

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,23 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // /about
 // /search
 // /home
+// /spring/framework
+// /spring/doc
+// /spring/learn
+
 // 위 경로에 다른 컴포넌트가 출력되도록 작성
 
 const router = createBrowserRouter([
+  { path: "/about", element: <div>about 컴포넌트</div> },
+  { path: "/search", element: <div>search 컴포넌트</div> },
+  { path: "/home", element: <div>home 컴포넌트</div> },
   {
-    path: "/about",
-    element: <div>about 컴포넌트</div>,
-  },
-  {
-    path: "/search",
-    element: <div>search 컴포넌트</div>,
-  },
-  {
-    path: "/home",
-    element: <div>home 컴포넌트</div>,
+    path: "/spring",
+    children: [
+      { path: "framework", element: <div>framework !!!</div> },
+      { path: "doc", element: <div>doc @@@</div> },
+      { path: "learn", element: <div>learn ###</div> },
+    ],
   },
 ]);
 
