b56bd99571de36542af88acff90fc1bb13f009e8
kwonyul <kw990812@gmail.com>
2024-05-17 15:40:13 +0900

    사이드바 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom"; // /react/doc
+
+// /react/doc
+// /react/tutorial
+// /react/sample
+
+function SpringBoot() {
+  return (
+    <div style={{ display: "flex", gap: "15px" }}>
+      <div
+        style={{
+          padding: "10px",
+          backgroundColor: "linen",
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+        }}
+      >
+        <div>
+          <Link to="/react/doc">DOC</Link>
+        </div>
+        <div>
+          <Link to="/react/tutorial">tutorial</Link>
+        </div>
+        <div>
+          <Link to="/react/sample">sample</Link>
+        </div>
+      </div>
+
+      <div style={{ flex: "1" }}>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/react",
+    element: <SpringBoot />,
+    children: [
+      { path: "doc", element: <div>react doc</div> },
+      { path: "tutorial", element: <div>react tutorial</div> },
+      { path: "sample", element: <div>react sample</div> },
+    ],
+  },
+]);
 
 function App(props) {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
