a07780e3de706f85b25b0c6121faa1399d149a69
kwonyul <kw990812@gmail.com>
2024-05-20 09:27:20 +0900

    chakra ui 버튼

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <div></div>
+      <div>
+        <Button colorScheme={"gray"}>Gray</Button>
+        <Button colorScheme={"red"}>Lorem.</Button>
+        <Button colorScheme={"orange"}>Sunt.</Button>
+        <Button colorScheme={"yellow"}>Nam.</Button>
+        <Button colorScheme={"green"}>Cum?</Button>
+        <Button colorScheme={"teal"}>Maiores.</Button>
+        <Button colorScheme={"blue"}>Eligendi!</Button>
+        <Button colorScheme={"cyan"}>Esse.</Button>
+        <Button colorScheme={"purple"}>Iusto?</Button>
+        <Button colorScheme={"pink"}>Ex.</Button>
+        <hr />
+        <Button colorScheme={"blue"} isLoading={true}>
+          Lorem.
+        </Button>
+        <Button colorScheme={"blue"} variant={"solid"}>
+          Lorem.
+        </Button>
+        <Button colorScheme={"blue"} variant={"outline"}>
+          Voluptate?
+        </Button>
+        <Button colorScheme={"blue"} variant={"ghost"}>
+          Reprehenderit!
+        </Button>
+        <Button colorScheme={"blue"} variant={"link"}>
+          Consectetur!
+        </Button>
+      </div>
     </ChakraProvider>
   );
 }
