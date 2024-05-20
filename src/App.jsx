0b3e207d9905984570db97a116e3ddaae0eaaeaa
kwonyul <kw990812@gmail.com>
2024-05-20 15:03:04 +0900

    기본 동작 막기 연습완료
    preventDefault

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // a 클릭 시
+  // form 서브밋 시
+
+  // 기본 동작 막고,, 콘솔에 간단한 메시지 출력
+
+  function handleLinkClick(e) {
+    e.preventDefault();
+    console.log("링크 기본동작 막음");
+  }
+
+  function handleSubmitForm(e) {
+    e.preventDefault();
+    console.log("폼 기본동작 막음");
+  }
+
+  return (
+    <div>
+      <a href="https://www.google.com" onClick={handleLinkClick}>
+        구글
+      </a>
+      <hr />
+      <form action="https://www.google.com" onSubmit={handleSubmitForm}>
+        <input type="text" />
+        <button>전송</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
