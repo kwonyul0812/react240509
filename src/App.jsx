2660e6661b24fd070d1560cfff86e6b768639eb5
kwonyul <kw990812@gmail.com>
2024-05-20 14:28:55 +0900

    이벤트 버블링 막기 연습 완료
    event bubbling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Badge, Box, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  function handleBoxClick(e) {
+    console.log("박스 클릭");
+  }
+
+  function handleButtonClick(e) {
+    e.stopPropagation();
+    console.log("버튼 클릭");
+  }
+
+  function handleBadgeClick(e) {
+    e.stopPropagation();
+    console.log("배지 클릭");
+  }
+
+  return (
+    <ChakraProvider>
+      <Box onClick={handleBoxClick} w={"200px"} bgColor={"gray"} p={1}>
+        <Button onClick={handleButtonClick} colorScheme={"blue"}>
+          클릭
+          <Badge onClick={handleBadgeClick} ml={3}>
+            new /
+          </Badge>
+        </Button>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
